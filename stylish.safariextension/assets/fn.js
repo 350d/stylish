96df82abcae66e2f4b9770b3b117d90b2fcf4c67
Vladimir Sobolev <v.sobolev@gmail.com>
2012-08-25 04:12:35 -0400

    First commit
    extension files

---
+++ stylish.safariextension/assets/fn.js
@@ -0,0 +1,124 @@
+version = '1.3.4';
+
+DB = {
+	set: function(name, data) {	
+		localStorage.setItem(name,data);
+	},
+	get: function(name) {
+		return localStorage.getItem(name);
+	},
+	delete: function(name) {
+		localStorage.removeItem(name);
+	},
+	clear: function() {
+		localStorage.clear();
+	},
+	size: function() {
+		return localStorage.length;
+	},
+	key: function(i) {
+		return localStorage.key(i);
+	}
+}
+
+function filterSection(href, section) {
+	if (!section.urls && !section.urlPrefixes && !section.domains && !section.regexps) return true;
+	var found = false,
+		currentDomains = getDomains(href);
+	if (section.urls) {
+		section.urls.forEach(function(url) {
+			if (url == href) {
+				found = true;
+				return;
+			}
+		});
+	}
+	if (section.urlPrefixes && !found) {
+		section.urlPrefixes.forEach(function(urlPrefix) {
+			if (href.indexOf(urlPrefix) == 0) {
+				found = true;
+				return;
+			}
+		});
+	}
+	if (section.domains && !found) {
+		section.domains.forEach(function(domain) {
+			if (currentDomains.indexOf(domain) >= 0) {
+				found = true;
+				return;
+			}
+		});
+	}
+	if (section.regexps && !found) {
+		section.regexps.forEach(function(regexp) {
+			if ((new RegExp(regexp)).test(href)) {
+				found = true;
+				return;
+			}
+		});
+	}
+	return found ? true : false;
+}
+
+function getHost(url) {
+    var a = document.createElement('a');
+    a.href = url;
+    return a.hostname.replace('www.','');
+}
+
+function getDomains(url) {
+	var d = getHost(url),
+		domains = [d];
+	while (d.indexOf('.') != -1) {
+		d = d.substring(d.indexOf('.') + 1);
+		domains.push(d);
+	}
+	return domains;
+}
+
+function pingAll(name, data) {
+	safari.application.browserWindows.forEach(function(win) {
+		win.tabs.forEach(function(tab) {
+			if (tab.page) tab.page.dispatchMessage(name, data);
+		})
+	})
+}
+
+function navInit() {
+	document.getElementById('version').innerHTML = version;
+	$('#menu nav a').click(function(event) {
+		event.preventDefault();
+		var a = $(this), url = a.attr('rel');
+		if (!a.hasClass('active')) window.location = safari.extension.baseURI + url + ".html";
+		return false;
+	})
+};
+
+JSON.stringify = JSON.stringify || function (obj) {
+    var t = typeof (obj);
+    if (t != "object" || obj === null) {
+        if (t == "string") obj = '"'+obj+'"';
+        return String(obj);
+    }
+    else {
+        var n, v, json = [], arr = (obj && obj.constructor == Array);
+        for (n in obj) {
+            v = obj[n]; t = typeof(v);
+            if (t == "string") v = '"'+v+'"';
+            else if (t == "object" && v !== null) v = JSON.stringify(v);
+            json.push((arr ? "" : '"' + n + '":') + String(v));
+        }
+        return (arr ? "[" : "{") + String(json) + (arr ? "]" : "}");
+    }
+};
+
+String.prototype.hashCode = function() {
+	var hash = 0;
+	if (this.length == 0) return hash;
+	for (i = 0; i < this.length; i++) {
+		char = this.charCodeAt(i);
+		hash = ((hash<<5)-hash)+char;
+		hash = hash & hash;
+	}
+	return hash;
+}
---
+++ stylish.safariextension/assets/global.js
@@ -0,0 +1,133 @@
+var usss = 'http://userstyles.org/styles/browse/', href,
+	w = window,
+	page;
+	
+$.ajaxSetup({
+    timeout: 10000,
+    error: function(event, request, settings) {
+		console.error('Ajax: ' + request);
+	}
+});
+
+function ping(event, name, data) {
+	if ( page = event.target.page) {
+		page.dispatchMessage(name, data);
+	}
+}
+
+function pong(event) {
+	var n = event.name,
+		t = event.target,
+		m = event.message,
+		l;
+		
+	switch(n) {
+		case 'saveStyle':
+			saveData(m.id,m.json);
+			pingAll('applyStyle', {"id":m.id});
+		break;
+		case 'disableStyle':
+			disableStyle(m.id);
+			pingAll('removeStyle', {"id":m.id});
+		break;
+		case 'enableStyle':
+			enableStyle(m.id);
+			pingAll('applyStyle', {"id":m.id});
+		break;
+		case 'updateStyle':
+			updateStyle(m.id, m.json);
+			pingAll('updateStyle', {"id":m.id,"css":m.json});
+		break;
+		case 'editStyle':
+			pingAll('editStyle', {"id":m.id,"json":DB.get(m.id)});
+		break;
+		case 'deleteStyle':
+			deleteStyle(m.id);
+			pingAll('removeStyle', {"id":m.id});
+		break;
+		case 'getInstalledStyles':
+			if (l = DB.size()) {
+				var list = [];
+				for (var i=0;i<l;i++) {
+					list.push(
+						{'id':DB.key(i),'json':DB.get(DB.key(i))}
+					);
+				}
+				ping(event, 'setInstalledStyles', list);
+			}
+		break;
+		case 'getStyles':
+			if (l = DB.size()) {
+				for (var i=0;i<l;i++) {
+					var json = $.parseJSON(DB.get(DB.key(i))),
+						id = DB.key(i),
+						filter, css;
+					if (json.enabled) {
+						if (filter = json.sections.filter(function(section) { return filterSection(m,section)})) {
+							if (css = filter.map(function(section) {return section.code;}).join("\n")) {
+								ping(event, 'injectStyle', {"css":css, "id":id, "location":m});
+							}
+						}
+					}
+				}
+			}
+		break;
+		case 'applyStyle':
+			var json = $.parseJSON(DB.get(m.id)),
+				filter, css;
+			if (json.enabled) {
+				if (filter = json.sections.filter(function(section) { return filterSection(m.href,section)})) {
+					if (css = filter.map(function(section) {return section.code;}).join("\n")) {
+						ping(event, 'injectStyle', {"css":css, "id":m.id, "location":m.href});
+					}
+				}
+			}
+		break;
+	}
+}
+
+function disableStyle(id) {
+	var json = $.parseJSON(DB.get(id));
+	json.enabled = false;
+	DB.set(id,JSON.stringify(json));
+}
+
+function enableStyle(id) {
+	var json = $.parseJSON(DB.get(id));
+	json.enabled = true;
+	DB.set(id,JSON.stringify(json));	
+}
+
+function updateStyle(id,data) {
+	DB.set(id,JSON.stringify(data));
+}
+
+function deleteStyle(id) {
+	DB.delete(id);
+}
+
+function getStyles(url, page) {
+
+}
+
+function manageStyles() {
+
+}
+
+function saveData(id,data) {
+	data.enabled = true;
+	DB.set(id,JSON.stringify(data));
+}
+
+function getHost(url) {
+    var a = document.createElement('a'), host;
+    a.href = url;
+	host = a.hostname.replace('www.','');
+    return host;
+}
+
+function log(l) {
+	console.log(l);
+};
+
+safari.application.addEventListener("message", pong, true);
